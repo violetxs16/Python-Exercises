ec07d0a502772d90bbabe750a4ed7de3b3ae2723
Violeta Solorio <violeta.solorio16@gmail.com>
2024-04-09 14:57:33 -0700

    Modified problem 1 & added test cases

--- Continous array problem.py
+++ Continous array problem.py
@@ -3,7 +3,7 @@
 
 #Keep track of overall sum of 5 current element
 #Keep track of start and end positions to maintain k elements per avg
-def avg_subarrays(k, arr):
+def avg_subarrays(arr, k):
     result = []
     sum = 0
     start = 0
@@ -13,8 +13,13 @@ def avg_subarrays(k, arr):
     for end in range(len(arr)):
         sum += arr[end]
         if(end >= k - 1): #if k = 5, condition will be 5-1 = 4, so end must be 4 for condition to be true
-            result[start] = sum / k
+            result.append(sum / k)
             sum -= arr[start] #We are subtracting the element we are passing
             start += 1
+    return result
 
-               
+arr = [2, 3, 4, 1, 5]
+print(avg_subarrays(arr, 2))
+arr = [2, 1, 5, 1, 3, 2]
+print(avg_subarrays(arr, 3))
+           
