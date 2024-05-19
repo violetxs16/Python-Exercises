e7f5f8f0d0a7dd89ffa05eb183663c02d8b36fc1
Violeta Solorio <violeta.solorio16@gmail.com>
2024-05-19 11:45:14 -0700

    Debugging: problem with output

--- Continous array problem 5.py
+++ Continous array problem 5.py
@@ -9,3 +9,36 @@
 #you will stop when you have to pick from a third fruit type.
 
 #Solution: Sliding window approach
+
+def fruit_into_baskets(fruits):
+    current_sum = 0
+    max_sum = 0
+    start_index = 0
+    prev_fruit = 0
+    #Count all the individual fruit frequencies 
+    fruit_freq = {}
+    for i in range(len(fruits)):
+        if fruits[i] not in fruit_freq:#Fruit not present
+            fruit_freq[fruits[i]] = 1
+        else:#Fruit already present
+            fruit_freq[fruits[i]] += 1
+    
+    for i in range(len(fruits)):
+        if i > 0 and fruits[prev_fruit] == fruits[i]: #Must have two distinct fruits
+            start_index += 1
+            continue
+
+        current_sum += fruit_freq[fruits[i]] #Add current fruit value
+        prev_fruit = i #New old fruit value
+        if i > 2:
+            current_sum -= fruit_freq[fruits[start_index]]
+            if current_sum > max_sum:
+                max_sum = current_sum
+            start_index += 1
+    return max_sum
+
+Fruit=['A', 'B', 'C', 'A', 'C']
+print(fruit_into_baskets(Fruit))
+
+Fruit2=['A', 'B', 'C', 'B', 'B', 'C']
+print(fruit_into_baskets(Fruit2))
