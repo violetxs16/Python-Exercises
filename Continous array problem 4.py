3a44d60c5fb74640686215092d311f36c89f37fc
Violeta Solorio <violeta.solorio16@gmail.com>
2024-05-19 11:10:29 -0700

    Finished solution for continous array problem 4

--- Continous array problem 4.py
+++ Continous array problem 4.py
@@ -2,14 +2,12 @@
 #approach: sliding window approach
 #Use: dictionary
 
-def k_distinct_chars(str , k):
+def k_distinct_chars(str_1 , k):
     distinct_chars = {}
     # We will add the characters to the dictionary & increase values if duplicates are found
     distinct_substr = ""
-    start_distinct = 0
-    end_distinct = 0
+    greatest_distinct_subtr = ""
     index_start = 0
-    num_distinct = 0
     #Step 1: Iterate through all characters of str
     #Step 2: Append to the distinct_substr & character to dictionary
     #           ---we access str[index_start] to remove char from dictionary
@@ -17,28 +15,28 @@ def k_distinct_chars(str , k):
     #We need to check dictionary values to see number of distinct characters in current substring
     #We need to record highest value & compare to new dictionary value when char gets inserted
     #if value is greater we update start_distinct & end_distinct values
-    for i in range(len(str)):
-        if str[i] not in distinct_chars:
-            distinct_chars[str[i]] = 1 #Add new char
+    for i in range(len(str_1)):
+        if str_1[i] not in distinct_chars:
+            distinct_chars[str_1[i]] = 1 #Add new char
         else:
-            distinct_chars[str[i]] += 1 #Increase value
+            distinct_chars[str_1[i]] += 1 #Increase value
 
-        distinct_substr += str[i]
-        if len(distinct_chars) > k:
-            distinct_chars[str[index_start]] -= 1 #Decrease char value
-            distinct_substr = distinct_substr[1:] #Cut out first char
+        distinct_substr += str_1[i] #Append char to substr
+       
+        while(len(distinct_chars) > k ): #Keep decreasing number of chars until we have k number
+            distinct_chars[str_1[index_start]] -= 1
+            distinct_substr = distinct_substr[1:]
+            if distinct_chars[str_1[index_start]] == 0: #Delete key with value zero
+                del distinct_chars[str_1[index_start]]
             index_start += 1
-            total_unique = 0
-            for val in distinct_chars.values():
-                total_unique += val 
-            if total_unique > num_distinct:#There is more unique characters in current dictionary
-                #Keep track of str longest substring
-                start_distinct = index_start
-                end_distinct = i
-                num_distinct = total_unique
+        
+
+        if len(distinct_substr) > len(greatest_distinct_subtr):
+            greatest_distinct_subtr = distinct_substr
+
     
-    return str[start_distinct:end_distinct+1]
-str1 = "Umbrella"
-print(k_distinct_chars(str1, 2))
+    return greatest_distinct_subtr
+str1 = "cbbebi"
+print(k_distinct_chars(str1, 3))
 str2 = "hello"
 print(k_distinct_chars(str2, 3))
