ec30933eb6fc1724589cd81db0872c441d9259b4
Violeta Solorio <violeta.solorio16@gmail.com>
2024-07-17 15:31:17 -0700

    Solved: reverseString

--- Two pointers/Reversing-swapping/reverse-string.py
+++ Two pointers/Reversing-swapping/reverse-string.py
@@ -3,3 +3,12 @@
 #Two pointer approach
 #Write a function that reverses a string. The input string is given as an array of characters s.
 #You must do this by modifying the input array in-place with O(1) extra memory.
+def reverseString( s: List[str]) -> None:
+        start = 0
+        end = len(s) - 1
+        while(start < end):
+            temp = s[start]
+            s[start] = s[end]
+            s[end] = temp
+            start += 1
+            end -= 1
