72ee801d6cd7906efa37ff412e2cb3b22a7c6c54
Violeta Solorio <violeta.solorio16@gmail.com>
2024-07-19 20:30:36 -0700

    Debugging reverse words

--- Two pointers/Reversing-swapping/reverse-words.py
+++ Two pointers/Reversing-swapping/reverse-words.py
@@ -6,3 +6,22 @@
 #Return a string of the words in reverse order concatenated by a single space.
 #Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
 
+def reverseWords(s: str) -> str:
+        words = []
+        start_front = 0
+        start_back = 0
+        end_front = len(s) - 1
+        end_back = len(s) - 1
+        while(start_front < end_front):
+            #Move pointers foward until we find a new word
+            while(start_back == ' '):
+                 start_back += 1
+            while(start_front != ' '):
+                 start_front += 1
+            while(end_back == ' '):
+                 end_back -= 1
+            while(end_front != ' '):
+                 end_front -= 1
+            temp = s[start_back:start_front]
+            
+            
